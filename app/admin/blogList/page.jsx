5c60e2468cb3b220e7ea32f8e6c33f7bbd3eff07
aaskar rana <aaskar04rana@gmail.com>
2024-09-22 17:27:49 +0530

    Blog list added

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,8 +1,37 @@
+import BlogTAbleItem from '@/components/AdminComponents/BlogTableItem'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className='felx-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+      <h1>All blogs</h1>
+      <div className='relative h-[80vh] max-w-[850px] overflow-x-auto border border-gray-400'>
+        <table className='w-full text-sm text-gray-500'>
+          <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50'>
+            <tr>
+              <th scope='col' className='hidden sm:block px-6 py-3'>
+                Author Name
+
+              </th>
+              <th scope='col' className=' px-6 py-3'>
+                Blog Title
+
+              </th>
+              <th scope='col' className=' px-6 py-3'>
+                Blog Date
+
+              </th>
+              <th scope='col' className=' px-6 py-3'>
+                Action
+
+              </th>
+            </tr>
+          </thead>
+            <tbody>
+              <BlogTAbleItem/>
+            </tbody>
+        </table>
+      </div>
       
     </div>
   )
---
+++ components/AdminComponents/BlogTableItem.jsx
@@ -0,0 +1,25 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const BlogTableItem = ({authorImg,title,author}) => {
+  return (
+   <tr className='bg-white border-b'>
+    <th scope = 'row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+        <Image width={40} height={40} src={authorImg?authorImg:assets.profile_icon}/>
+        <p>{author?author:"No author"}</p>
+    </th>
+    <td className='px-6 py-4'>
+        {title?title:"no title"}
+    </td>
+    <td className='px-6 py-4'>
+        {"22 spetember 2024"}
+    </td>   
+    <td className='px-6 py-4 cursor-pointer'>
+        x
+    </td>
+   </tr>
+  )
+}
+
+export default BlogTableItem
